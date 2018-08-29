22e5eb7032876fb4023ff30d6807e79cfce3f6a0
Tramac <1145955920@qq.com>
2018-08-29 11:44:54 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from gensim.models import word2vec
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    sentences = word2vec.LineSentence("wiki_seg.txt")
+    model = word2vec.Word2Vec(sentences, size=250)
+
+    # 保存模型
+    model.save("word2vec.model")
+
+    # 模型读取
+    # model = word2vec.Word2Vec.load("word2vec.model")
+
+
+if __name__ == "__main__":
+    main()
